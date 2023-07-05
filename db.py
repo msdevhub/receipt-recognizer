5fb3c4dc386cd63e76c68b405fc92320b87b9303
LewayHackintosh <michael.wei@foxmail.com>
2023-07-05 23:48:10 +0800

    publish

--- db.py
+++ db.py
@@ -2,6 +2,119 @@ import pymysql
 import os
 
 
+def get_pdf_files():
+    """
+    获取PDF文件列表
+    """
+    # 建立数据库连接
+    connection = pymysql.connect(
+        host=os.getenv("DB_HOST"),
+        port=int(os.getenv("DB_PORT")),
+        database=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+    )
+
+    # 创建游标对象
+    cursor = connection.cursor()
+
+    # 查询PDF表中的文件列表及其对应的 PDF ID
+    query_files = "SELECT id, filename FROM pdfs"
+    cursor.execute(query_files)
+    result = cursor.fetchall()
+    files = [(row[1], row[0]) for row in result]
+
+
+    # 关闭游标和连接
+    cursor.close()
+    connection.close()
+
+    return files
+
+
+def get_receipts_by_pdf_id(pdf_id):
+    """
+    根据 PDF ID 获取收据信息列表
+    """
+    # 建立数据库连接
+    connection = pymysql.connect(
+        host=os.getenv("DB_HOST"),
+        port=int(os.getenv("DB_PORT")),
+        database=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+    )
+
+    # 创建游标对象
+    cursor = connection.cursor()
+
+    # 查询指定 PDF ID 的收据信息
+    query_receipts = "SELECT * FROM receipts WHERE pdf_id = %s"
+    cursor.execute(query_receipts, (pdf_id,))
+    result = cursor.fetchall()
+
+    # 关闭游标和连接
+    cursor.close()
+    connection.close()
+
+    return result
+
+def get_receipt_items_by_pdf_id(pdf_id):
+    # 建立数据库连接
+    connection = pymysql.connect(
+        host=os.getenv("DB_HOST"),
+        port=int(os.getenv("DB_PORT")),
+        database=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+    )
+
+    # 创建游标对象
+    cursor = connection.cursor()
+
+    # 查询指定 PDF ID 的收据项
+    query = """
+    SELECT *
+    FROM receipt_items
+    WHERE pdf_id = %s
+    """
+    cursor.execute(query, (pdf_id,))
+    receipt_items = cursor.fetchall()
+
+    # 关闭游标和连接
+    cursor.close()
+    connection.close()
+
+    return receipt_items
+
+
+def get_receipt_items_by_receipt_id(receipt_id):
+    """
+    根据收据 ID 获取收据项信息列表
+    """
+    # 建立数据库连接
+    connection = pymysql.connect(
+        host=os.getenv("DB_HOST"),
+        port=int(os.getenv("DB_PORT")),
+        database=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+    )
+
+    # 创建游标对象
+    cursor = connection.cursor()
+
+    # 查询指定收据 ID 的收据项信息
+    query_receipt_items = "SELECT * FROM receipt_items WHERE receipt_id = %s"
+    cursor.execute(query_receipt_items, (receipt_id,))
+    result = cursor.fetchall()
+
+    # 关闭游标和连接
+    cursor.close()
+    connection.close()
+
+    return result
+
 def insert_data_to_mysql(data_list, pdf_filename, pdf_filepath):
     # 建立数据库连接
     connection = pymysql.connect(
@@ -59,6 +172,7 @@ def insert_data_to_mysql(data_list, pdf_filename, pdf_filepath):
     create_items_table_query = """
     CREATE TABLE IF NOT EXISTS receipt_items (
       id INT PRIMARY KEY AUTO_INCREMENT,
+      pdf_id INT,
       receipt_id INT,
       Description VARCHAR(255),
       Quantity DECIMAL(10, 2),
@@ -84,32 +198,41 @@ def insert_data_to_mysql(data_list, pdf_filename, pdf_filepath):
         INSERT INTO receipts (pdf_id, ReceiptType, MerchantName, TransactionDate, Subtotal, TotalTax, Tip, Total)
         VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
         """
-        cursor.execute(insert_receipt_query, (
-            pdf_id,
-            data.get("ReceiptType"),
-            data.get("MerchantName"),
-            data.get("TransactionDate"),
-            data.get("Subtotal", 0) if data.get("Subtotal") != "" else 0,
-            data.get("TotalTax", 0) if data.get("TotalTax") != "" else 0,
-            data.get("Tip", 0) if data.get("Tip") != "" else 0,
-            data.get("Total", 0) if data.get("Total") != "" else 0,
-        ))
+        cursor.execute(
+            insert_receipt_query,
+            (
+                pdf_id,
+                data.get("ReceiptType"),
+                data.get("MerchantName"),
+                data.get("TransactionDate"),
+                data.get("Subtotal", 0) if data.get("Subtotal") != "" else 0,
+                data.get("TotalTax", 0) if data.get("TotalTax") != "" else 0,
+                data.get("Tip", 0) if data.get("Tip") != "" else 0,
+                data.get("Total", 0) if data.get("Total") != "" else 0,
+            ),
+        )
         receipt_id = cursor.lastrowid
 
         # 插入收据项信息
         items = data.get("Items", [])
         for item in items:
             insert_item_query = """
-            INSERT INTO receipt_items (receipt_id, Description, Quantity, Price, TotalPrice)
-            VALUES (%s, %s, %s, %s, %s)
+            INSERT INTO receipt_items (pdf_id, receipt_id, Description, Quantity, Price, TotalPrice)
+            VALUES (%s, %s, %s, %s, %s, %s)
             """
-            cursor.execute(insert_item_query, (
-                receipt_id,
-                item.get("Description"),
-                item.get("Quantity", 0),
-                item.get("Price", 0),
-                item.get("TotalPrice", 0),
-            ))
+            cursor.execute(
+                insert_item_query,
+                (
+                    pdf_id,
+                    receipt_id,
+                    item.get("Description", ""),
+                    item.get("Quantity", 0) if item.get("Quantity", "") != "" else 0,
+                    item.get("Price", 0) if item.get("Price", "") != "" else 0,
+                    item.get("TotalPrice", 0)
+                    if item.get("TotalPrice", "") != ""
+                    else 0,
+                ),
+            )
 
     # 提交事务
     connection.commit()
--- main.py
+++ main.py
@@ -1,30 +1,17 @@
 import streamlit as st
-import pandas as pd
 import os
+import pandas as pd
 from formrecognizer import AzureFormRecognizerClient
-from db import insert_data_to_mysql
+from db import get_pdf_files, get_receipt_items_by_pdf_id, insert_data_to_mysql, get_receipts_by_pdf_id, get_receipt_items_by_receipt_id
 
 pdf_parser = AzureFormRecognizerClient()
-
-def read_upload_file(file_path):
-    """
-    读取并显示给定路径的CSV文件
-    """
-    df = pd.read_csv(file_path)
-    st.write("上传的数据帧：")
-    st.write(df)
-    st.write("表格：")
-    st.table(df)
 
 def main():
     st.set_page_config(page_title="上传和显示表格", layout="wide")
     st.sidebar.title("侧边栏")
 
-    # 获取data目录中的CSV文件列表
-    files = []
-    for filename in os.listdir("./data"):
-        # if filename.endswith(".csv"):
-        files.append(filename)
+    # 获取PDF文件列表
+    files = get_pdf_files()
 
     # 显示文件列表
     st.sidebar.subheader("上传的文件列表")
@@ -32,13 +19,34 @@ def main():
         st.sidebar.write("还没有上传任何文件")
     else:
         # 使用radio组件显示文件列表
-        selected_file = st.sidebar.radio("选择要查看的文件", files)
-        file_path = os.path.join("./data", selected_file)
-        read_upload_file(file_path)
+        selected_file, selected_file_id = st.sidebar.radio("选择要查看的文件", files, format_func=lambda file: file[0])
+
+        # 显示选择的文件信息
+        st.sidebar.write(f"选择的文件：{selected_file}，{selected_file_id}")
+
+        # 获取选择文件的收据信息和收据项信息
+        receipts = get_receipts_by_pdf_id(selected_file_id)
+        receipt_items = get_receipt_items_by_pdf_id(selected_file_id)
+
+        # 显示收据信息
+        st.write("收据信息：")
+        if len(receipts) == 0:
+            st.write("没有找到收据信息")
+        else:
+            receipts_df = pd.DataFrame(receipts, columns=["ID", "PDF ID", "Receipt Type", "Merchant Name", "Transaction Date", "Subtotal", "Total Tax", "Tip", "Total"])
+            st.table(receipts_df)
+
+        # 显示收据项信息
+        st.write("收据项信息：")
+        if len(receipt_items) == 0:
+            st.write("没有找到收据项信息")
+        else:
+            receipt_items_df = pd.DataFrame(receipt_items, columns=["ID","PDF ID", "Receipt ID", "Description", "Quantity", "Price", "Total Price"])
+            st.table(receipt_items_df)
 
     # 上传文件
     st.sidebar.subheader("选择要上传的文件")
-    uploaded_file = st.sidebar.file_uploader("上传CSV文件", type=["pdf","jpg","jpeg", "csv"])
+    uploaded_file = st.sidebar.file_uploader("上传PDF文件", type=["pdf", "jpg", "jpeg"])
     if uploaded_file is not None:
         # 检查文件名是否已存在
         if uploaded_file.name in files:
@@ -48,16 +56,10 @@ def main():
             with open(os.path.join("./data", uploaded_file.name), "wb") as f:
                 file_value = uploaded_file.getvalue()
                 values = pdf_parser.analyze_read(file_value)
-                df = pd.json_normalize(values, ['Items'], ['MerchantName', 'TransactionDate', 'Total', 'Subtotal', 'TotalTax', 'Tip'])
-                st.write(df)
-                print(df)
-                print(values)
-                insert_data_to_mysql(values)
+                insert_data_to_mysql(values, uploaded_file.name, os.path.join("./data", uploaded_file.name))
                 f.write(file_value)
             # 自动刷新侧边栏中的文件列表
-            # st.experimental_rerun()
-            # 显示上传的文件
-            # read_upload_file(uploaded_file)
+            st.experimental_rerun()
 
 if __name__ == '__main__':
     main()
